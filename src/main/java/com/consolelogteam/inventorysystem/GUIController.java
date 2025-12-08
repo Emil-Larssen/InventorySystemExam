bd5ae532944d5f40eedad9e2f796c27a2f1a2894
josef <josefinebechfrandsen@gmail.com>
2025-12-08 11:23:19 +0100

    i Item - tilføjet getItemName og toString. i InventoryManager - oprettet observable list med et eksemple item. Og udkommenteret putItemInInventory. i GUIController - tilføjet ListView, TextField, Button, InventoryManager.

--- src/main/java/com/consolelogteam/inventorysystem/GUIController.java
+++ src/main/java/com/consolelogteam/inventorysystem/GUIController.java
@@ -1,14 +1,36 @@
 package com.consolelogteam.inventorysystem;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class GUIController {
     @FXML
-    private Label welcomeText;
+    private ListView<Item> inventoryListView;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField selectedItemTextField;
+
+    @FXML
+    private Button removeItemOnClick;
+
+    private final InventoryManager inventoryManager = new InventoryManager();
+
+    @FXML
+    public void initialize() {
+        inventoryListView.setItems(inventoryManager.getAllItems());
+
+        inventoryListView.getSelectionModel()
+                .selectedItemProperty()
+                .addListener((obs, oldItem, newItem) -> {
+                    if (newItem != null) {
+                        selectedItemTextField.setText("Valgt frugt: " + newItem.getItemName());
+                    } else {
+                        selectedItemTextField.setText("Ingen frugt valgt");
+                    }
+                });
+
+        selectedItemTextField.setText("Ingen frugt valgt");
     }
 }
--- src/main/java/com/consolelogteam/inventorysystem/InventoryManager.java
+++ src/main/java/com/consolelogteam/inventorysystem/InventoryManager.java
@@ -1,10 +1,18 @@
 package com.consolelogteam.inventorysystem;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class InventoryManager {
     Inventory inventory = new Inventory();
 
-    private void putItemInInventory(String name) {
-        inventory.addItem(name);
+    public ObservableList<Item> getAllItems() {
+        return FXCollections.observableArrayList(
+                new Item ("Example", 2.3)
+        );
+
+    //private void putItemInInventory(String name) {
+    //    inventory.addItem(name);
     }
     //removeItemFromInventory(){}
 
