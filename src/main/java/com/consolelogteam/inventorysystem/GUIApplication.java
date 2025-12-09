bf0abfb7752fa5196d7f54fd65eae00f48ef0538
Emil-larssen <emillarssne@gmail.com>
2025-12-09 10:24:07 +0100

    Increased UI window start size

--- src/main/java/com/consolelogteam/inventorysystem/GUIApplication.java
+++ src/main/java/com/consolelogteam/inventorysystem/GUIApplication.java
@@ -11,7 +11,7 @@ public class GUIApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GUIApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 650, 500);
         stage.setTitle("InventorySystem");
         stage.setScene(scene);
         stage.show();
